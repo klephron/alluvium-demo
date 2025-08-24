d4544710f8724aab36f1f16d1730ae06f3ce9c78
Nikita Kulakov <klephron.dev@gmail.com>
2025-08-24 11:18:51 +0300

    add bridges between vms

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,9 @@ locals {
   pools_path_prefix  = "/opt/alluvium/demo"
   pools_disks        = "alluvium-demo-disks"
   pools_images       = "alluvium-demo-images"
+  net_1              = "alluvium-net-1"
+  net_2              = "alluvium-net-2"
+  net_3              = "alluvium-net-3"
 }
 
 module "vm" {
@@ -20,78 +23,94 @@ module "vm" {
       }
     }
   }
+  bridges = {
+    "${local.net_1}" = {
+      bridge = "br-alluvium-1"
+    }
+    "${local.net_2}" = {
+      bridge = "br-alluvium-2"
+    }
+    "${local.net_3}" = {
+      bridge = "br-alluvium-3"
+    }
+  }
   vms = {
-    ubuntu = {
-      net-1-r = {
-        pools = {
-          images = local.pools_images
-          disks  = local.pools_disks
-        }
-        base = {
-          source = local.ubuntu_base_source
-        }
-        vcpu      = 1
-        memory    = 1024
-        disk_size = 30
-        hostfwd = [
-        ]
+    alluvium-demo-1-r = {
+      pools = {
+        images = local.pools_images
+        disks  = local.pools_disks
       }
-      net-1-c = {
-        pools = {
-          images = local.pools_images
-          disks  = local.pools_disks
-        }
-        base = {
-          source = local.ubuntu_base_source
-        }
-        vcpu      = 1
-        memory    = 1024
-        disk_size = 30
-        hostfwd = [
-        ]
+      base = {
+        source = local.ubuntu_base_source
       }
-      net-2-r = {
-        pools = {
-          images = local.pools_images
-          disks  = local.pools_disks
-        }
-        base = {
-          source = local.ubuntu_base_source
-        }
-        vcpu      = 1
-        memory    = 1024
-        disk_size = 30
-        hostfwd = [
-        ]
+      vcpu      = 1
+      memory    = 1024
+      disk_size = 30
+      bridges = [
+        local.net_1,
+        local.net_3
+      ]
+    }
+    alluvium-demo-1-c = {
+      pools = {
+        images = local.pools_images
+        disks  = local.pools_disks
       }
-      net-2-c = {
-        pools = {
-          images = local.pools_images
-          disks  = local.pools_disks
-        }
-        base = {
-          source = local.ubuntu_base_source
-        }
-        vcpu      = 1
-        memory    = 1024
-        disk_size = 30
-        hostfwd = [
-        ]
+      base = {
+        source = local.ubuntu_base_source
       }
-      net-3-c = {
-        pools = {
-          images = local.pools_images
-          disks  = local.pools_disks
-        }
-        base = {
-          source = local.ubuntu_base_source
-        }
-        vcpu      = 1
-        memory    = 1024
-        disk_size = 30
-        hostfwd = [
-        ]
+      vcpu      = 1
+      memory    = 1024
+      disk_size = 30
+      bridges = [
+        local.net_1
+      ]
+    }
+    alluvium-demo-2-r = {
+      pools = {
+        images = local.pools_images
+        disks  = local.pools_disks
       }
+      base = {
+        source = local.ubuntu_base_source
+      }
+      vcpu      = 1
+      memory    = 1024
+      disk_size = 30
+      bridges = [
+        local.net_2,
+        local.net_3
+      ]
+    }
+    alluvium-demo-2-c = {
+      pools = {
+        images = local.pools_images
+        disks  = local.pools_disks
+      }
+      base = {
+        source = local.ubuntu_base_source
+      }
+      vcpu      = 1
+      memory    = 1024
+      disk_size = 30
+      bridges = [
+        local.net_2
+      ]
+    }
+    alluvium-demo-3-c = {
+      pools = {
+        images = local.pools_images
+        disks  = local.pools_disks
+      }
+      base = {
+        source = local.ubuntu_base_source
+      }
+      vcpu      = 1
+      memory    = 1024
+      disk_size = 30
+      bridges = [
+        local.net_3
+      ]
     }
   }
 }
